b6c3333fe1ce01e07c945b8cf74ae5dc4c281d48
Azitrea <xyzddz@gmail.com>
2022-05-27 13:15:10 +0200

    readme and cors

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Basic Deno server test with oak
+
+mysql: sudo service mysql start
+run: deno run --allow-net src/main.ts
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./router/router.ts";
 import db from "./denodb-database.service.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 const app = new Application();
 const port: number = 8000;
 
+app.use(
+  oakCors({
+    origin: 'http://localhost:8100'
+  }),
+)
 app.use(router.prefix('/api').routes());
 app.use(router.allowedMethods());
+
 
 await db.sync({ drop: true });
 
